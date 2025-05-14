8905254eef7c3dd5526edfd4c57e57d2cb5507a9
Budgingcobra <Prathitbhat6@gmail.com>
2025-05-14 13:17:24 +0100

    Making Duplex

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include <iostream>
+
 
 // Define RS485 communication pins
-const int RS_RO = 10; // Pin to receive data (RO on MAX485)
+const int RS_RO = 7; // Pin to receive data (RO on MAX485)
 const int RS_DI = 8; // Pin to transmit data (DI on MAX485)
-const int RS_DE_RE = 9; // Pin to enable RS485 transmission (DE/RE on MAX485)
+const int RS_DE_RE = 10; // Pin to enable RS485 transmission (DE/RE on MAX485)
 
 // Set up SoftwareSerial for RS485 communication
 SoftwareSerial RS_Master(RS_RO, RS_DI);
@@ -27,6 +27,13 @@ void loop() {
   // Ensure RS485 is in receive mode
   if (Serial.available())
   {
+    digitalWrite(RS_DE_RE, HIGH);
     RS_Master.write(Serial.read());
+    digitalWrite(RS_DE_RE, LOW);
+
+    if (RS_Master.available())
+    {
+      Serial.write(RS_Master.read());
+    }
   }
 }
